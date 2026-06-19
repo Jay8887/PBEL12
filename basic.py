fe1120301ce8c809e251086e40e537a74ed968fa
Jay Tiwari <jaytiwari@Jays-MacBook-Pro.local>
2026-06-19 15:19:05 +0530

    1.Added Python data types examples

--- basic.py
+++ basic.py
@@ -1,1 +1,29 @@
-print("Hello Devdatt")
+# variable
+
+a = 10
+print(a)
+print(type(a))
+
+a = 10.5
+print(type(a))
+
+a = "Hello"
+print(type(a))
+
+a = True
+print(type(a))
+
+a = 1+1j
+print(type(a))
+
+a = (1, 2, 3, 4)
+print(type(a))
+
+a = [1, 2, 3, 4]
+print(type(a))
+
+a = {1, 2, 3, 4}
+print(type(a))
+
+a = {1: "one", 2: "two", 3: "three"}
+print(type(a))
